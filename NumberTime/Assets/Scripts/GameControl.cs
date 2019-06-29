60eb85fbd5d2306d65daa4e6531ea41687b23e2a
R1msky <33123095+R1msky@users.noreply.github.com>
2019-06-29 17:57:43 +0300

    first steps

---
+++ NumberTime/Assets/Scripts/GameControl.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using Unity;
+
+namespace TMPro
+{
+    public class GameControl : MonoBehaviour
+    {
+        private Timer timer;
+        private int timeSize = 333;
+        private int count = 0;
+        private int _score = 0;
+        private int numberOfObjects = 1;
+        private float width;
+        private float height;
+
+        [SerializeField] private float offsetX = 0.2f;
+        private float scaleRatio = 0.75f;
+        [SerializeField] private GameObject prefCube;
+        [SerializeField] private GameObject newCube;
+        [SerializeField] private List<float> xOffsets = new List<float>() {1f, 0f, 2f};
+        [SerializeField] private List<float> yOffsets = new List<float>() {1f, 1f, 1f};
+        [SerializeField] private List<GameObject> cubes;
+       // [SerializeField] private List<Vector3> positions = {new Vector3() };
+        Animation animation;
+        
+        void Start()
+        {
+            width = Camera.main.pixelWidth;
+            height = Camera.main.pixelHeight;
+            timer = gameObject.GetComponent<Timer>();
+            animation = gameObject.GetComponent<Animation>();
+           
+        }
+
+        void Update()
+        {
+
+        }
+        
+        void OnMouseDown()
+        {
+            if (timer.getTime() < 100 && timer.getTime() > 0)
+            {
+                rightAction();
+                
+            }
+
+            
+            else
+            {
+                wrongAction();
+            }
+        }
+
+        private void rightAction()
+        {
+            count++;
+            timer.setTime(timeSize);
+            _score = timer.getTime();
+            animation.Play("Rotation");
+            if (count > 2)
+            {
+                timeSize += 150;
+                count = 0;
+                timer.setTime(timeSize);
+                spawnCube();
+
+            }
+            Debug.Log("+");
+        }
+
+        private void wrongAction()
+        {
+            Time.timeScale = 0;
+            Debug.Log("-");
+        }
+
+        private int i = 0; 
+
+        private void spawnCube()
+        {
+            newCube = (Instantiate(prefCube as GameObject));
+           // cubes.Add(Instantiate(prefCube as GameObject));
+           // cubes[i].transform.position = new Vector3(xOffsets[i], yOffsets[i], transform.position.z);
+           
+            //transform.position = new Vector3(-xOffsets[i], yOffsets[i]);
+          
+            newCube.transform.position = transform.position;
+          
+            //cubes[i].transform.localScale *= scaleRatio;
+            transform.localScale *= scaleRatio;
+            newCube.transform.localScale *= scaleRatio;
+           // if (i == 2)
+           // {
+           //     scaleRatio -= 0.25f;
+           // }
+          //  i++;
+           transform.Translate(new Vector3(transform.position.x - offsetX, 0));
+           newCube.transform.Translate(new Vector3(newCube.transform.position.x + offsetX, 0)); 
+        }
+    }
+}
---
+++ NumberTime/Assets/Scripts/Timer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TMPro {
+
+    public class Timer : MonoBehaviour
+    {
+
+
+        [SerializeField] private TMP_Text _text;
+        [SerializeField] private int time;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            //_text = gameObject.GetComponent<Canvas>();
+            time = 333;
+
+            _text.text = "" + time;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (time > 0)
+            {
+                time = (int)((time - Time.deltaTime * 100f));
+                _text.text = "" + time;
+            }
+            else
+            {
+                
+            }
+        }
+
+
+        public int getTime()
+        {
+            return time;
+        }
+
+        public void setTime(int time)
+        {
+            if (time > 0)
+            this.time = time;
+        }
+
+    }
+}
